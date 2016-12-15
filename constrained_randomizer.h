206f71c1886ca0a7ba5d216278a6aa024402df20
Yitzi2 <ytkornbluth@hotmail.com>
2016-12-15 13:30:27 -0500

    Update constrained_randomizer.h

--- constrained_randomizer.h
+++ constrained_randomizer.h
@@ -1,7 +1,7 @@
-/*This function takes a budget, a number of items, and an array of costs, and generates an unbiased random selection 
-    from among the valid arrangements of items (with validity defined as in the unbounded knapsack problem; in particular, 
-    any item can occur multiple times).  The results are output to the fourth argument, and the fifth argument optionally 
-    allows for usage of a different random number generator.*/
+/*This function takes a budget, a number of items, and an array of strictly positivecosts, and generates an unbiased random 
+    selection from among the valid arrangements of items (with validity defined as in the unbounded knapsack problem; in 
+    particular, any item can occur multiple times).  The results are output to the fourth argument, and the fifth argument 
+    optionally allows for usage of a different random number generator.*/
 
 #ifndef CONSTRAINED_RANDOMIZER_H
 #define CONSTRAINED_RANDOMIZER_H
